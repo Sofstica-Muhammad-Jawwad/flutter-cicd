29942392096813b4ad37d1a338edf85045fb89c6
Muhammad Jawwad <muhammad.jawwad@sofstica.com>
2024-12-19 12:51:36 +0500

    Hellow orld

--- lib/main.dart
+++ lib/main.dart
@@ -58,12 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
@@ -73,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times: ehalkdlakhdjkasldjasdlaksjdlakjsldkajslajslkj',
+              'You have pushed',
             ),
             Text(
               '$_counter',
